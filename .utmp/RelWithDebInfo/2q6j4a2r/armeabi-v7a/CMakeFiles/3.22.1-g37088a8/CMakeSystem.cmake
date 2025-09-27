f80259b5ccfbfeda64c46b6cdf7c0cf679e0b064
zeds <tom.zed39@gmail.com>
2025-09-28 01:14:49 +0530

    production

--- .utmp/RelWithDebInfo/2q6j4a2r/armeabi-v7a/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
+++ .utmp/RelWithDebInfo/2q6j4a2r/armeabi-v7a/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Darwin-24.4.0")
+set(CMAKE_HOST_SYSTEM "Darwin-24.6.0")
 set(CMAKE_HOST_SYSTEM_NAME "Darwin")
-set(CMAKE_HOST_SYSTEM_VERSION "24.4.0")
+set(CMAKE_HOST_SYSTEM_VERSION "24.6.0")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/Applications/Unity/Hub/Editor/6000.1.0f1/PlaybackEngines/AndroidPlayer/NDK/build/cmake/android.toolchain.cmake")
